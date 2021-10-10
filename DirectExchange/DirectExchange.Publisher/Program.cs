3986d1634ab2589c26b78be73a3dc059b07473fe
kerem <kerem@phonoclick.com>
2021-10-10 16:26:32 +0300

    direct exchange

---
+++ DirectExchange/DirectExchange.Publisher/Program.cs
@@ -0,0 +1,57 @@
+﻿using RabbitMQ.Client;
+using Shared.RabbitMQ;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace DirectExchange.Publisher
+{
+  class Program
+  {
+    public enum LogNames
+    { 
+      Critical = 1,
+      Error = 2,
+      Warning = 3,
+      Info = 4
+    }
+
+    static void Main(string[] args)
+    {
+      var channel = RabbitMQUtil.GetChannel();
+
+      channel.ExchangeDeclare("logs-direct", ExchangeType.Direct, true);
+
+      Publish(channel);
+
+      Console.ReadLine();
+    }
+
+    static void Publish(IModel channel)
+    {
+      Enum.GetNames(typeof(LogNames)).ToList().ForEach(x =>
+      {
+        var routeKey = $"route-{x}";
+        var queueName = $"direct-queue-{x}";
+        channel.QueueDeclare(queueName, true, false, false);
+
+        channel.QueueBind(queueName, "logs-direct", routeKey, null);
+      });
+
+      Enumerable.Range(1, 50).ToList().ForEach(x =>
+      {
+        var log = (LogNames)new Random().Next(1, 5);
+
+        var message = $"log-type: {log}";
+
+        var messageBody = Encoding.UTF8.GetBytes(message);
+
+        var routeKey = $"route-{log}";
+
+        channel.BasicPublish("logs-direct", routeKey, null, messageBody);
+
+        Console.WriteLine($"Log gönderilmiştir: {message}");
+      });
+    }
+  }
+}
---
+++ DirectExchange/DirectExchange.Subscriber/Program.cs
@@ -0,0 +1,42 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using Shared.RabbitMQ;
+using System;
+using System.IO;
+using System.Text;
+using System.Threading;
+
+namespace DirectExchange.Subscriber
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var channel = RabbitMQUtil.GetChannel();
+
+      channel.BasicQos(0, 1, false);
+
+      var consumer = new EventingBasicConsumer(channel);
+
+      var queueName = "direct-queue-Critical";
+
+      channel.BasicConsume(queueName, false, consumer);
+
+      Console.WriteLine("Log dinleniyor...");
+
+      consumer.Received += (object sender, BasicDeliverEventArgs e) =>
+      {
+        var message = Encoding.UTF8.GetString(e.Body.ToArray());
+        Console.WriteLine(message);
+
+        Thread.Sleep(1000);
+
+        //File.AppendAllText("log-critical.txt", message + "\n");
+
+        channel.BasicAck(e.DeliveryTag, false);
+      };
+
+      Console.ReadLine();
+    }
+  }
+}
