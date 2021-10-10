b1b212db8cc9c1ebecc3d12c5737242a356849c8
kerem <kerem@phonoclick.com>
2021-10-10 17:10:26 +0300

    header exchange

---
+++ HeaderExchange/HeaderExchange.Publisher/Program.cs
@@ -0,0 +1,33 @@
+﻿using RabbitMQ.Client;
+using Shared.RabbitMQ;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HeaderExchange.Publisher
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var channel = RabbitMQUtil.GetChannel();
+
+      channel.ExchangeDeclare("header-exchange", ExchangeType.Headers, true);
+
+      var headers = new Dictionary<string, object>
+      {
+        { "format", "pdf" },
+        { "shape2", "a4" }
+      };
+
+      var properties = channel.CreateBasicProperties();
+      properties.Headers = headers;
+
+      channel.BasicPublish("header-exchange", "", properties, Encoding.UTF8.GetBytes("Header mesajım."));
+
+      Console.WriteLine("Mesaj gönderilmiştir.");
+      Console.ReadLine();
+    }
+
+  }
+}
---
+++ HeaderExchange/HeaderExchange.Subscriber/Program.cs
@@ -0,0 +1,50 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using Shared.RabbitMQ;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace HeaderExchange.Subscriber
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var channel = RabbitMQUtil.GetChannel();
+
+      channel.BasicQos(0, 1, false);
+
+      var consumer = new EventingBasicConsumer(channel);
+
+      var queueName = channel.QueueDeclare().QueueName;
+
+      var headers = new Dictionary<string, object>
+      {
+        { "format", "pdf" },
+        { "shape", "a4" },
+        //{ "x-match", "all" }
+        { "x-match", "any" }
+      };
+
+      channel.QueueBind(queueName, "header-exchange", string.Empty, headers);
+
+      channel.BasicConsume(queueName, false, consumer);
+
+      Console.WriteLine("Log dinleniyor...");
+
+      consumer.Received += (object sender, BasicDeliverEventArgs e) =>
+      {
+        var message = Encoding.UTF8.GetString(e.Body.ToArray());
+        Console.WriteLine(message);
+
+        Thread.Sleep(1000);
+
+        channel.BasicAck(e.DeliveryTag, false);
+      };
+
+      Console.ReadLine();
+    }
+  }
+}
