a8e588f53633052b307db0c2f380acc5b75ea470
kerem <kerem@phonoclick.com>
2021-10-11 22:44:50 +0300

    create rabbitmq client service

---
+++ src/Watermark/Watermark.WebApp/Services/RabbitMQClientService.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Extensions.Logging;
+using RabbitMQ.Client;
+using System;
+
+namespace Watermark.WebApp.Services
+{
+  public class RabbitMQClientService : IDisposable
+  {
+    private readonly ILogger<RabbitMQClientService> _logger;
+
+    private readonly ConnectionFactory _connectionFactory;
+    private IConnection _connection;
+    private IModel _channel;
+
+    public const string ExchangeName = "ImageDirectExchange";
+    public const string RoutingWatermark = "watermark-route-image";
+    public const string QueueName = "queue-watermark-image";
+
+    public RabbitMQClientService(ILogger<RabbitMQClientService> logger, ConnectionFactory connectionFactory)
+    {
+      _logger = logger;
+      _connectionFactory = connectionFactory;
+    }
+
+    public IModel Connect()
+    {
+      _connection = _connectionFactory.CreateConnection();
+
+      if (_channel is { IsOpen: true })
+      {
+        return _channel;
+      }
+
+      _channel = _connection.CreateModel();
+      _channel.ExchangeDeclare(ExchangeName, ExchangeType.Direct, true, false);
+      _channel.QueueDeclare(QueueName, true, false, false, null);
+
+      _channel.QueueBind(QueueName, ExchangeName, RoutingWatermark);
+
+      _logger.LogInformation("RabbitMQ ile bağlantı kuruldu.");
+
+      return _channel;
+    }
+
+    public void Dispose()
+    {
+      _channel?.Close();
+      _channel?.Dispose();
+
+      _connection?.Close();
+      _connection?.Dispose();
+
+      _logger.LogInformation("RabbitMQ ile bağlantı koptu.");
+    }
+
+  }
+}
--- src/Watermark/Watermark.WebApp/Startup.cs
+++ src/Watermark/Watermark.WebApp/Startup.cs
@@ -5,11 +5,13 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using RabbitMQ.Client;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Watermark.WebApp.Models;
+using Watermark.WebApp.Services;
 
 namespace Watermark.WebApp
 {
@@ -25,6 +27,10 @@ namespace Watermark.WebApp
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
+      services.AddSingleton(sp => new ConnectionFactory() { Uri = new Uri(Configuration.GetConnectionString("RabbitMQ")) });
+
+      services.AddSingleton<RabbitMQClientService>();
+
       services.AddDbContext<AppDbContext>(options =>
       {
         options.UseInMemoryDatabase(databaseName: "productdb");
